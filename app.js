3ac771bf50d37e816913003087c9a9a90d908f8c
sonali_sohoni <ssohoni@camsbycbs.com>
2021-10-12 14:29:54 -0400

    used npm-inquirer module

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const fs = require("fs");
-const generatePage = require("./src/page-template.js");
-console.log(generatePage);
-//Assignment destructuring
-const [name1, gitHub] = profileDataArgs;
+const inquirer = require("inquirer");
+inquirer
+	.prompt([
+		{
+			type: "input",
+			name: "name",
+			message: "What is your name?",
+		},
+	])
+	.then((answers) => console.log(answers));
 
-fs.writeFile("index.html", generatePage(name1, gitHub), (err) => {
-	if (err) throw err;
-	console.log("Portfolio complete checkout index.html");
-});
+// const fs = require("fs");
+// const generatePage = require("./src/page-template.js");
+// console.log(generatePage);
+// //Assignment destructuring
+// const [name1, gitHub] = profileDataArgs;
+
+// fs.writeFile("index.html", generatePage(name1, gitHub), (err) => {
+// 	if (err) throw err;
+// 	console.log("Portfolio complete checkout index.html");
+// });
